ac5bed6f70b0862c3cf45831bc7f66b9d908b7b8
Yusuf Uzun <uzunyusuf@yahoo.com>
2020-04-10 03:39:34 +0300

    applied changes according to issue #6

--- DbscanDemo/Program.cs
+++ DbscanDemo/Program.cs
@@ -8,28 +8,44 @@ namespace DbscanDemo
     {
         static void Main(string[] args)
         {
-            MyCustomDatasetItem[] featureData = { };
+            MyCustomFeature[] featureData = { };
 
-            List<MyCustomDatasetItem> testPoints = new List<MyCustomDatasetItem>();
-            for (int i = 0; i < 1000; i++)
+            var testPoints = new List<MyCustomFeature>();
+
+            //points around (1,1) with most 1 distance
+            testPoints.Add(new MyCustomFeature(1, 1));
+            for (int i = 1; i <= 1000; i++)
             {
-                //points around (1,1) with most 1 distance
-                testPoints.Add(new MyCustomDatasetItem(1, 1 + ((float)i / 1000)));
-                testPoints.Add(new MyCustomDatasetItem(1, 1 - ((float)i / 1000)));
-                testPoints.Add(new MyCustomDatasetItem(1 - ((float)i / 1000), 1));
-                testPoints.Add(new MyCustomDatasetItem(1 + ((float)i / 1000), 1));
-
-                //points around (5,5) with most 1 distance
-                testPoints.Add(new MyCustomDatasetItem(5, 5 + ((float)i / 1000)));
-                testPoints.Add(new MyCustomDatasetItem(5, 5 - ((float)i / 1000)));
-                testPoints.Add(new MyCustomDatasetItem(5 - ((float)i / 1000), 5));
-                testPoints.Add(new MyCustomDatasetItem(5 + ((float)i / 1000), 5));
+                float v = ((float)i / 1000);
+                testPoints.Add(new MyCustomFeature(1, 1 + v));
+                testPoints.Add(new MyCustomFeature(1, 1 - v));
+                testPoints.Add(new MyCustomFeature(1 - v, 1));
+                testPoints.Add(new MyCustomFeature(1 + v, 1));
             }
-            featureData = testPoints.ToArray();
-            HashSet<MyCustomDatasetItem[]> clusters;
 
-            var dbs = new DbscanAlgorithm<MyCustomDatasetItem>((x, y) => Math.Sqrt(((x.X - y.X) * (x.X - y.X)) + ((x.Y - y.Y) * (x.Y - y.Y))));
-            dbs.ComputeClusterDbscan(allPoints: featureData, epsilon: .01, minPts: 10, clusters: out clusters);
+            //points around (5,5) with most 1 distance
+            testPoints.Add(new MyCustomFeature(5, 5));
+            for (int i = 1; i <= 1000; i++)
+            {
+                float v = ((float)i / 1000);
+                testPoints.Add(new MyCustomFeature(5, 5 + v));
+                testPoints.Add(new MyCustomFeature(5, 5 - v));
+                testPoints.Add(new MyCustomFeature(5 - v, 5));
+                testPoints.Add(new MyCustomFeature(5 + v, 5));
+            }
+
+            featureData = testPoints.ToArray();
+
+            //INFO: applied euclidean distance as metric calculation function
+            var dbscan = new DbscanAlgorithm<MyCustomFeature>(
+                (feature1, feature2) =>
+                Math.Sqrt(
+                        ((feature1.X - feature2.X) * (feature1.X - feature2.X)) +
+                        ((feature1.Y - feature2.Y) * (feature1.Y - feature2.Y))
+                    )
+                );
+
+            var result = dbscan.ComputeClusterDbscan(allPoints: featureData, epsilon: .01, minimumPoints: 10);
         }
     }
 }
---
+++ DbscanImplementation/DbscanResult.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DbscanImplementation
+{
+    /// <summary>
+    /// Result object of algorithm after clusters computed
+    /// </summary>
+    /// <typeparam name="TFeature">Feature data contribute into algorithm</typeparam>
+    public class DbscanResult<TFeature>
+    {
+        public DbscanResult(DbscanPoint<TFeature>[] allPoints)
+        {
+            var allClusters = allPoints
+                .GroupBy(x => x.ClusterId)
+                .ToDictionary(x => x.Key, x => x.ToArray());
+
+            Clusters = new Dictionary<int, DbscanPoint<TFeature>[]>(allClusters.Where(x => x.Key > 0));
+            Noise = allClusters.ContainsKey((int)ClusterIds.Noise) ? allClusters[(int)ClusterIds.Noise] : new DbscanPoint<TFeature>[0];
+            Unclassified = allClusters.ContainsKey((int)ClusterIds.Unclassified) ? allClusters[(int)ClusterIds.Unclassified] : new DbscanPoint<TFeature>[0];
+        }
+
+        public Dictionary<int, DbscanPoint<TFeature>[]> Clusters { get; }
+
+        public DbscanPoint<TFeature>[] Unclassified { get; }
+
+        public DbscanPoint<TFeature>[] Noise { get; set; }
+    }
+}
