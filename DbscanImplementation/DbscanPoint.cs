b193c59588f8cf42161c8c5c3a21b19170606cd4
Yusuf Uzun <uzunyusuf@yahoo.com>
2020-04-16 04:49:41 +0300

    ClusterId removed and refactoring added for issue #15

--- DbscanImplementation/DbscanPoint.cs
+++ DbscanImplementation/DbscanPoint.cs
@@ -8,18 +8,15 @@ namespace DbscanImplementation
     {
         public TFeature Feature { get; internal set; }
 
-        public bool IsVisited { get; internal set; }
+        public int? ClusterId { get; internal set; }
 
-        public int ClusterId { get; internal set; }
-
-        public PointType PointType { get; internal set; }
+        public PointType? PointType { get; internal set; }
 
         public DbscanPoint(TFeature feature)
         {
             Feature = feature;
-            IsVisited = false;
-            ClusterId = (int)DbscanImplementation.ClusterId.Unclassified;
-            PointType = PointType.Unclassified;
+            ClusterId = null;
+            PointType = null;
         }
     }
 }
--- DbscanImplementation/PointType.cs
+++ DbscanImplementation/PointType.cs
@@ -2,11 +2,6 @@ namespace DbscanImplementation
 {
     public enum PointType
     {        
-        /// <summary>
-        /// This Id shows when a point is IsVisited value equals to false.
-        /// </summary>
-        Unclassified = -1,
-
         Noise = 0,
         Core = 1,
         Border = 2
